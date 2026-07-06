b880395fc75d6eb2a644bc99461bdda79568699f
KingRosa19 <annelieserosa@icloud.com>
2026-07-06 19:06:39 -0400

    Update firebase config to use environment variables

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,7 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"
 import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
