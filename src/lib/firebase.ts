d1d429e2b1c762234d4ba568509b04581a6500b1
KingRosa19 <annelieserosa@icloud.com>
2026-07-06 19:12:34 -0400

    Intial firebase update

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"
 import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
-export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+const firebaseConfig = {
+  apiKey: "AIzaSyC-awsCpb6OvHY42seyunumnmJt4rgOJ2Q",
+  authDomain: "summarist-ab14c.firebaseapp.com",
+  projectId: "summarist-ab14c",
+  storageBucket: "summarist-ab14c.firebasestorage.app",
+  messagingSenderId: "905721880804",
+  appId: "1:905721880804:web:79d0c01b98e2051a448cf1"
 };
 
 // Initialize Firebase
